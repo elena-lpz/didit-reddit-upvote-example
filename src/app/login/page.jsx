3aa4071e8df517bfdd2db796291fd075480236aa
Elena Lopez <emloppal@gmail.com>
2025-07-09 14:58:14 +0100

    fix: redirect issue

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -1,10 +1,16 @@
 import { LoginButton } from "@/components/LoginButton";
 import Link from "next/link";
+import { auth } from "@/auth";
+import { redirect } from "next/navigation";
 
 //TODO handle login error
 //This solution is consistent with similar actions like trying to add a post when not logged in. This page is reusable, so it could substitute the error when adding a post, and could be reused for any other action that requires logging in
 
-export default function LogIn() {
+export default async function LogIn() {
+  const session = await auth();
+  if (session) {
+    redirect("/");
+  }
   return (
     <>
       <Link
--- src/components/LoginButton.jsx
+++ src/components/LoginButton.jsx
@@ -1,11 +1,12 @@
 import { signIn } from "@/auth";
-
+//did this first
 //https://www.reddit.com/r/nextjs/comments/1cnnimy/is_it_possible_to_redirect_on_signin_with/ with some tweaks
+//then realise this changed the redirect for everything and we might not want that so tried something different )see login page)
 
 export function LoginButton() {
   async function handleLogin() {
     "use server";
-    await signIn("credentials", { redirectTo: "/" });
+    await signIn();
   }
 
   return (
